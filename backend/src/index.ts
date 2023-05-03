f6947826081bb2e3b0a639b24f675538e300390b
conorfennell <fennell.conor@gmail.com>
2023-05-03 20:42:44 +0100

    return all users on root path

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,11 +7,12 @@ interface Env {
   
 const app = new Hono<{ Bindings: Env }>()
 
-app.get('/', (c) => c.text('Hello Cloudflare Workers!'))
+app.get('/', async (c) => c.json(await c.env.database.prepare('SELECT * FROM users;').all<User>()))
 app.get('/v1/users/:id', async c => {
     const id = Number.parseInt(c.req.param('id'));
-
+    console.log(id);
     const user = await c.env.database.prepare('SELECT * FROM users WHERE id = ? LIMIT 1').bind(id).first<User>();
+    console.log(JSON.stringify(user));
     return c.json(user);
 });
 
