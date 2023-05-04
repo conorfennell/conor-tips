0cd249c42ebe595fbd9680c3686a661109df8444
conorfennell <fennell.conor@gmail.com>
2023-05-04 22:06:36 +0100

    add call to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
+  const [user, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://backend.fennell-conor1617.workers.dev/v1/users/1')
+      .then(response => response.json())
+      .then(user => setData(user));
+  }, []);
 
   return (
     <>
-      <h1>Conor's tips</h1>
+      {user && <h1>{user.firstName}'s tips</h1>}
       <ul>
         <li><a href="https://www.amazon.com/Shokz-OpenRun-Pro-Conduction-Built/dp/B09BVXT8TJ">Bone conducting headphones</a></li>
       </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
